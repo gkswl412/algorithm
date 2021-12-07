94c1a58ac13b3285b7a63cc9f253d7ff755d14b7
gkswl412 <gkswl412@naver.com>
2021-12-07 21:15:29 +0900

    makeStar.py 1차

--- algo/makeStar.py
+++ algo/makeStar.py
@@ -1,9 +1,21 @@
-n = int(input())
 
-for i in range(0,n):
-    for j in range(0,n):
-        if(i==1 and j==1):
-            print(" ", end="")
-        else:
-            print("*", end="")
-    print("")
+def star(n: int, x: list)-> list:
+    out = [] #한번 처리한 값
+    if n == 3:
+        return x
+    else:
+        for i in x: # 상단 3개의 구역
+            out.append(i*3)
+        for i in x: # 중단 3개의 구역
+            out.append(i+' '*len(x)+i)
+        for i in x: # 하단 3개의 구역
+            out.append(i*3)
+        return star(n//3, out)
+
+if __name__== "__main__":
+    n = int(input())
+    first = ['***', '* *', '***']
+    final = star(n, first)
+    print(final)
+    for i in final:
+        print(i)
