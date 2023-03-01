6c3ce45b49ee2b7a7af1ce036e7515d94baffa6d
Jeonghyeon Park <gkswl412@naver.com>
2023-03-01 15:45:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,26 +1,12 @@
-box = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-def group_word_checker(word):
-  for alphabet in box:
-    start = 0
-    end = 0
-    if alphabet in word:
-      start = word.index(alphabet) + 1
-    
-    for i in range(start,len(word)):
-      if word[i] != alphabet:
-        for j in range(i+1,len(word)):
-          if word[j] == alphabet:
-            return False
-            
-  return True
-
 data_count = int(input())
-group_word_count = 0
+result = data_count
+for i in range(data_count):
+  word = input()
+  for j in range(0,len(word)-1):
+      if word[j] == word[j+1]:
+        pass
+      elif word[j] in word[j+1:]:
+        result -= 1
+        break
 
-for i in range(0,data_count):
-  data = input()
-  if group_word_checker(data):
-    group_word_count += 1
-
-print(group_word_count)
+print(result)
