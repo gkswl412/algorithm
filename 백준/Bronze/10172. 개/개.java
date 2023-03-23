d06d2fadebb094952af363d5b0731afa460e0fa7
Jeonghyeon Park <gkswl412@naver.com>
2023-03-23 23:10:51 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+    
+    public void solution() {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
