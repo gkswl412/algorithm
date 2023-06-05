aaa7dad77eededa9fc7e744eaa0bc331836ed82d
Jeonghyeon Park <gkswl412@naver.com>
2023-06-04 23:45:18 -0700

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        
+        long n = Long.parseLong(br.readLine());
+        
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
