d571267a4379d09e900e7fbb1dfd124c990acf36
Jeonghyeon Park <gkswl412@naver.com>
2023-06-11 00:47:04 -0700

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -10,6 +10,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         
         int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
         
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
@@ -40,17 +41,15 @@ public class Main {
         } */
         
         // 삽입 정렬
-        for(int i=1; i<n; i++) {
-            for(int j=i; j>0; j--) {
-                if(arr[j-1] > arr[j]) {
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }else {
-                    break;
-                }
-            }
-        }
+        for(int index = 1 ; index < arr.length ; index++){ // 1.
+          int temp = arr[index];
+          int prev = index - 1;
+          while( (prev >= 0) && (arr[prev] > temp) ) {    // 2.
+             arr[prev+1] = arr[prev];
+             prev--;
+          }
+          arr[prev + 1] = temp;                           // 3.
+       }
         
         for(int i=0; i<n; i++) {
             sb.append(arr[i]).append("\n");
