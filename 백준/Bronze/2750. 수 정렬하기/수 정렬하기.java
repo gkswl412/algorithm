29aba4830a4ad75e6239e7a9b7d0e89b198bbc82
Jeonghyeon Park <gkswl412@naver.com>
2023-06-24 23:14:35 -0700

    [Bronze II] Title: 수 정렬하기, Time: 156 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,5 +1,6 @@
 import java.io.*;
-import java.util.*;
+import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,52 +9,27 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         int n = Integer.parseInt(br.readLine());
-        
-        int[] arr = new int[n];
-        List<Integer> list = new ArrayList<>();
+        Integer[] arr = new Integer[n];
         
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         
-        // 버블 정렬
-        /* for(int i=0; i<n-1; i++) {
-            for(int j=1; j<n-i; j++) {
-                if (arr[j-1] > arr[j]) {
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        } */
-        
-        // 선택 정렬
-        /* for(int i=0; i<n-1; i++) {
-            int minValueIndex = i;
-            for(int j=i; j<n; j++) {
-                if (arr[j] < arr[minValueIndex]) {
-                    minValueIndex = j;
-                }
-            }
+        // 삽입 정렬 (Insertion Sort)
+        for (int i=1; i<n; i++) {
             int temp = arr[i];
-            arr[i] = arr[minValueIndex];
-            arr[minValueIndex] = temp;
-        } */
-        
-        // 삽입 정렬
-        for(int index = 1 ; index < arr.length ; index++){ // 1.
-          int temp = arr[index];
-          int prev = index - 1;
-          while( (prev >= 0) && (arr[prev] > temp) ) {    // 2.
-             arr[prev+1] = arr[prev];
-             prev--;
-          }
-          arr[prev + 1] = temp;                           // 3.
-       }
-        
-        for(int i=0; i<n; i++) {
-            sb.append(arr[i]).append("\n");
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > temp) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = temp;
         }
+        
+        
+        Stream<Integer> stream = Arrays.stream(arr);
+        
+        stream.forEach(i -> sb.append(i).append("\n"));
         
         System.out.println(sb);
     }
