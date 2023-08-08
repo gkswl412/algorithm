1e3b00dfd2c1fb8d6d11b229d2fb1e9140342d3b
Jeonghyeon Park <gkswl412@naver.com>
2023-08-08 05:56:11 -0700

    [Silver V] Title: 수 정렬하기 2, Time: 948 ms, Memory: 243428 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        new Main().mergeSort(arr, 0, n-1);
+        
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    
+    public void merge(int[] arr, int left, int mid, int right) {
+        int[] leftArr = Arrays.copyOfRange(arr, left, mid+1);
+        int[] rightArr = Arrays.copyOfRange(arr, mid+1, right+1);
+        
+        int i = 0, j = 0, k = left;
+        
+        while(i<leftArr.length && j<rightArr.length) {
+            if (leftArr[i] > rightArr[j]) {
+                arr[k] = rightArr[j++];
+            } else {
+                arr[k] = leftArr[i++];
+            }
+            k++;
+        }
+        
+        while(i < leftArr.length) {
+            arr[k++] = leftArr[i++];
+        }
+        
+        while(j < rightArr.length) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+}
