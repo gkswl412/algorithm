8266b22e31f56b373eb0d70bc07c5ed43eaa8273
Jeonghyeon Park <gkswl412@naver.com>
2023-08-08 03:52:46 -0700

    [Bronze II] Title: 대표값2, Time: 76 ms, Memory: 11320 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,31 +1,42 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
-    
+        
         int[] arr = new int[5];
-    
-        for(int i=0; i<5; i++) {
+        
+        for (int i=0; i<arr.length; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-    
-        // 삽입 정렬 (n = 5)
-        for (int i=1; i<5; i++) {
-            int temp = arr[i];
-            int prev = i-1;
-            while (prev>=0 && (arr[prev] > temp)) {
-                arr[prev+1] = arr[prev];
-                prev--;
-            }
-            arr[prev+1] = temp;
+        
+        new Main().sort(arr);
+        
+        int sum = 0;
+        
+        for (int i=0; i<arr.length; i++) {
+            sum += arr[i];
         }
-    
-        int average = Arrays.stream(arr).sum() / 5;
-    
+        
+        int average = sum / 5;
+        int middleValue = arr[2];
+        
         System.out.println(average);
-        System.out.println(arr[2]);
+        System.out.println(middleValue);
     }
+    
+    public void sort(int[] arr) {
+        for (int i=0; i<arr.length; i++) {
+            for (int j=1; j<arr.length-i; j++) {
+                if (arr[j-1] > arr[j]) {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    
+    
 }
