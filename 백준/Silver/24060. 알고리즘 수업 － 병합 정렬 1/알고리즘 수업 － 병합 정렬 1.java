429fe7441a8dd7cbd60d45d0e83c142cd971e3c8
Jeonghyeon Park <gkswl412@naver.com>
2023-10-17 00:23:12 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 676 ms, Memory: 153960 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -2,42 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int count = 0;
-	static int k = 0;
-	static int result = -1;
+    public static int count = 0;
+    public static int k = 0;
+    public static int result = -1;
+    
     public static void main(String[] args) throws IOException {
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
-        StringBuilder sb = new StringBuilder();
         
-        String[] input = br.readLine().split(" ");
+        String[] firstInputLine = br.readLine().split(" ");
+        String[] secondInputLine = br.readLine().split(" ");
         
-        int n = Integer.parseInt(input[0]);
-        k = Integer.parseInt(input[1]);
-     
+        int n = Integer.parseInt(firstInputLine[0]);
+        k = Integer.parseInt(firstInputLine[1]);
         int[] arr = new int[n];
         
-        String[] row = br.readLine().split(" ");
-        
         for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(row[i]);
+            arr[i] = Integer.parseInt(secondInputLine[i]);
         }
         
         new Main().mergeSort(arr, 0, n-1);
-        
-        for(int i=0; i<n; i++) {
-            sb.append(arr[i]).append("\n");
-        }
         
         System.out.println(result);
     }
     
     public void mergeSort(int[] arr, int left, int right) {
-        if ( left < right ) {
-        	int mid = ( left + right ) / 2;
-        	mergeSort(arr, left, mid);
-        	mergeSort(arr, mid + 1, right);
-        	merge(arr, left, mid, right);
+        if(left < right) {
+            int mid = (left + right)/2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+            merge(arr, left, mid, right);
         }
     }
     
@@ -47,36 +41,36 @@ public class Main {
         
         int i=0, j=0, t=left;
         
-        while(i < leftArr.length && j < rightArr.length) {
-        	if (leftArr[i] > rightArr[j]) {
-        		count++;
-        		if (count == k) {
-        			result = rightArr[j];
-        		}
-        		arr[t++] = rightArr[j++];
-        	} else {
-        		count++;
-        		if (count == k) {
-        			result = leftArr[i];
-        		}
-        		arr[t++] = leftArr[i++];
-        	}
+        while(i<leftArr.length && j<rightArr.length) {
+            if(leftArr[i] < rightArr[j]) {
+                count++;
+                if(count == k) {
+                    result = leftArr[i];
+                }
+                arr[t++] = leftArr[i++];
+            }else {
+                count++;
+                if(count == k) {
+                    result = rightArr[j];
+                }
+                arr[t++] = rightArr[j++];
+            }
         }
         
-        while(i < leftArr.length) {
-        	count++;
-        	if (count == k) {
-    			result = leftArr[i];
-    		}
-        	arr[t++] = leftArr[i++];
+        while(i<leftArr.length) {
+            count++;
+            if(count == k) {
+                result = leftArr[i];
+            }
+            arr[t++] = leftArr[i++];
         }
         
-        while(j < rightArr.length) {
-        	count++;
-        	if (count == k) {
-    			result = rightArr[j];
-    		}
-        	arr[t++] = rightArr[j++];
+        while(j<rightArr.length) {
+            count++;
+            if(count == k) {
+                result = rightArr[j];
+            }
+            arr[t++] = rightArr[j++];
         }
     }
 }
